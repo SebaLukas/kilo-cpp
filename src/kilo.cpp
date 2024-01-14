1480e14f811a2339998f1b486075f7ee5ce9f537
Sebastian Lukas <sisasebbl@googlemail.com>
2024-01-14 18:58:32 +0100

    Added EnableRawMode, disableRawMode, restore terminal settings after exiting kilo
    Signed-off-by: Sebastian Lukas <sisasebbl@googlemail.com>

--- src/kilo.cpp
+++ src/kilo.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 
+#include <stdlib.h>
+#include <cstdint>
+#include <termios.h>
+#include <unistd.h>
+
+termios original_terminos;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminos);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &original_terminos);
+    atexit(disableRawMode);
+
+    termios raw = original_terminos;
+
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
 
-    std::cout << "Hallo Welt!";
+    enableRawMode();
+
+    uint8_t c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        std::cout << "Output: " << c << "\n";
+    };
 
     return 0;
 }
